a72118584b3b06420cb4ea9dd06cea5d6ec622c1
mateusdeitos <mate.deitos@hotmail.com>
2021-01-05 20:52:30 -0300

    Ajustes eslint

--- src/modules/user/services/CreateUserService.ts
+++ src/modules/user/services/CreateUserService.ts
@@ -1,20 +1,19 @@
-import User from "../entities/typeorm/User";
-import { IUserRepository } from "../repositories/dto/IUserRepository";
+import User from '../entities/typeorm/User';
+import { IUserRepository } from '../repositories/dto/IUserRepository';
 
 export default class CreateUserService {
+  constructor(private userRepository: IUserRepository) {}
 
-    constructor(
-        private userRepository: IUserRepository,
-    ) {}
+  private async validadeUser(user: User): Promise<void> {
+    const existeUserComMesmoEmail = await this.userRepository.findByProp(
+      'email',
+      user.email,
+    );
+  }
 
-    private async validadeUser(user: User): Promise<void> {
-        const existeUserComMesmoEmail = await this.userRepository.findByProp('email', user.email);
+  public async executa(user: User): Promise<User> {
+    this.validadeUser(user);
 
-    }
-
-    public async executa(user: User): Promise<User> {
-        this.validadeUser(user);
-
-        return this.userRepository.save(user);
-    }
+    return this.userRepository.save(user);
+  }
 }
