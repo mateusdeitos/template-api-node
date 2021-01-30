2470d5eec7d667dfd2b7cbb75cbb17e432e7de80
Mateus Campos Deitos <mate.deitos@hotmail.com>
2021-01-30 16:50:24 -0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,6 @@
 import { ConnectionOptions } from 'typeorm';
 import 'dotenv/config';
 
-const entities =
-  process.env.NODE_ENV === 'dev'
-    ? [String(process.env.TYPEORM_ENTITIES_LOCAL)]
-    : [String(process.env.TYPEORM_ENTITIES_DEPLOY)];
-
 const config: ConnectionOptions = {
   type: process.env.TYPEORM_CONNECTION as 'mariadb',
   host: process.env.TYPEORM_HOST,
@@ -15,7 +10,13 @@ const config: ConnectionOptions = {
   database: process.env.TYPEORM_DATABASE,
   connectTimeout: 60000,
   acquireTimeout: 60000,
-  entities,
+  entities: [
+    String(
+      process.env.NODE_ENV === 'dev'
+        ? process.env.TYPEORM_ENTITIES_DEV
+        : process.env.TYPEORM_ENTITIES,
+    ),
+  ],
   migrations: [String(process.env.TYPEORM_MIGRATIONS)],
   cli: {
     migrationsDir: String(process.env.TYPEORM_MIGRATIONS_DIR),
